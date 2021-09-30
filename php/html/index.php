361d437ca6722df783c6fe0f40b03cc271f8fdcb
antcha19 <you@example.com>
2021-09-30 11:40:01 +0200

    php

--- php/html/index.php
+++ php/html/index.php
@@ -1,19 +1,22 @@
-<?php require 'upload.php' ?>
+
+
+
 <html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
 <link href="imagenes.css" rel="stylesheet" type="text/css">
 <form method="post" enctype="multipart/form-data">
 
-
-
-   <input type="file" name="foto"><br>
-   <input type="submit" name="Enviar" value="Enviar">
-
+<a class='btn btn-primary' href='upload.php?'>Enviar imagenes</a>
+<a class='btn btn-primary' href='delete.php?'>Borrar imagenes</a>
 
    <?php
    $ruta = scandir("imagenes");
    for ($i = 0; $i < count($ruta); $i++) {
       if ($i > 1) {
+       //  echo "<input type=checkbox value=imagenes/$ruta[$i]> </input>";
          echo " <img src=imagenes/$ruta[$i]>";
+         
       }
    }
 
--- php/html/upload.php
+++ php/html/upload.php
@@ -1,18 +1,19 @@
 <?php
 if (isset($_POST['Enviar']) && $f = $_FILES['foto'] ?? []) {
-    
+
     if ($f['error'])
 
-        $error= "Error al subir  fichero";
+        $error = "Error al subir  fichero";
 
     else { //tenemos el fichero
 
         //subir fichero
-        $destino = 'imagenes/'.$f['name'];
+        $destino = 'imagenes/' . $f['name'];
+
         if (!move_uploaded_file($f['tmp_name'], $destino)) {
             $error = "error al mover el fichero";
         } else {
-            $error= "Subida correctamente";
+            $error = "Subida correctamente";
         }
     }
     echo "<pre>";
@@ -21,3 +22,30 @@ if (isset($_POST['Enviar']) && $f = $_FILES['foto'] ?? []) {
 }
 
 ?>
+
+
+
+<html>
+<link href="imagenes.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+<form method="post" enctype="multipart/form-data">
+
+    <?php
+
+    //   for ($i = 0; $i < 5; $i++) {
+    //     echo "<input type=file name=foto[]><br>";
+    //}
+    ?>
+    <input type=file name=foto><br>
+    <input type="submit" name="Enviar" value="Enviar">
+
+
+    <a class=btn btn-primary href=index.php?>Inicio</a>
+
+
+
+    <?= $error ?? '' ?>
+</form>
+
+</html>
