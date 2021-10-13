521872fa57ede636274acea3d1706ebf01fb233a
antcha19 <you@example.com>
2021-10-13 09:59:45 +0200

    raton

--- Desarrollo/javascript/raton/raton.js
+++ Desarrollo/javascript/raton/raton.js
@@ -20,22 +20,25 @@ window.onload = function () {
         //imprimo para comprobar 
         console.log(coordenadas);
 
-
+//posicion hacia la derecha
         if (x > pos_anteriorX && y == pos_anteriorY) {
             console.log("derecha");
             //cambiamos el estilo de la foto
             raton.style.left = x + "px";
         }
+        //posicion hacia la izquierda
         if (x < pos_anteriorX && y == pos_anteriorY) {
             console.log("izquierda");
             //cambiamos el estilo de la foto
             raton.style.left = x + "px";
         }
+        //posicion hacia la arriba
         if (x == pos_anteriorX && y > pos_anteriorY) {
             console.log("arriba");
             //cambiamos el estilo de la foto
             raton.style.top = y + "px";
         }
+        ////posicion hacia la abajo
         if (x == pos_anteriorX && y < pos_anteriorY) {
             console.log("abajo");
             //cambiamos el estilo de la foto
