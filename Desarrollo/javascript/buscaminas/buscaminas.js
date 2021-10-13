5ee9cf6901095df9fe5405133481cf91ace2f7c8
antcha19 <you@example.com>
2021-10-13 13:39:43 +0200

    liyftg67

--- Desarrollo/javascript/buscaminas/buscaminas.js
+++ Desarrollo/javascript/buscaminas/buscaminas.js
@@ -1,3 +1,22 @@
 window.onload = function () {
-    
+
+    var btnjugar = document.getElementById("jugar");
+    var cuadrado = document.getElementById("cuadrado");
+
+
+
+    btnjugar.addEventListener('click', () => {
+        console.log("click en jugar");
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                console.log("divI " + i + " divJ " + j)
+                let newdiv = document.createElement("div");
+                cuadrado.appendChild(newdiv);
+
+            }
+
+        }
+    })
+
+
 }
