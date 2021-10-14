fee205734c77201fac7a15d9ce69459a85ed4c51
antcha19 <you@example.com>
2021-10-14 14:57:46 +0200

    buscaminass

--- Desarrollo/javascript/buscaminas/buscaminas.js
+++ Desarrollo/javascript/buscaminas/buscaminas.js
@@ -2,21 +2,54 @@ window.onload = function () {
 
     var btnjugar = document.getElementById("jugar");
     var cuadrado = document.getElementById("cuadrado");
+    var filas_input = document.getElementById("filas");
+
+    var columas_input = document.getElementById("columas");
+
+
+
 
 
 
     btnjugar.addEventListener('click', () => {
+        //obtengo valores 
+        filasvalor = filas.value;
+        filascolumnas = columnas.value;
+      
+
+        tableto(filasvalor, filascolumnas);
+        obtnerid();
+
+    })
+
+
+    function tableto(filas, columnas) {
         console.log("click en jugar");
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < filas; i++) {
+         
+            for (let j = 0; j < columnas; j++) {
                 console.log("divI " + i + " divJ " + j)
-                let newdiv = document.createElement("div");
-                cuadrado.appendChild(newdiv);
+                let newdiv = document.createElement("button");
+                newdiv.id = i + "," + j;
+                cuadrado.appendChild(newdiv).innerText = "posicion " + i + j;
 
             }
 
         }
-    })
 
+    }
+
+    function obtnerid() {
+        //array de botones
+        var boton = document.getElementsByTagName("button");
+        var id = "";
+
+        for (var i = 0; i < boton.length; i++) {
+            id = boton[i].getAttribute('id');
+        }
+
+
+
+    }
 
 }
