340da6b462679ba2da8e716f75d3be90cf985bde
antcha19 <you@example.com>
2021-10-29 12:48:44 +0200

    dhsfgjfdsgh

---
+++ Desarrollo/javascript/checkbox/check.js
@@ -0,0 +1,39 @@
+var namegroup = document.getElementsByName("deporte");
+var lbel = document.getElementById("label");
+var comprobar = document.getElementById("comprobar");
+var contador = 0;
+
+
+for (let i = 0; i < namegroup.length; i++) {
+    namegroup[i].addEventListener('click', () => {
+        if (namegroup[i].checked) {
+
+            if (contador < 3) {
+                contador++
+                //   lbel.innerHTML = "has selecionado "+ contador;
+
+            } else {
+
+                // lbel.innerHTML = "solo puede selecionar 3";
+                namegroup[i].checked = false;
+            }
+
+        } else {
+            contador--;
+            //  lbel.innerHTML = "has selecionado "+ contador;
+        }
+
+    })
+}
+
+comprobar.addEventListener('click', () => {
+    for (let i = 0; i < namegroup.length; i++) {
+        //si esta selecionado , me nuestar el value
+        if (namegroup[i].checked) {
+            console.log(namegroup[i].value);
+            lbel.innerHTML += namegroup[i].value + " ";
+        }
+
+
+    }
+})
