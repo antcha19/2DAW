d0b355027200939029f6c67790ddafb265d0ff58
antcha19 <you@example.com>
2021-11-03 10:12:30 +0100

    sfgfh

--- Desarrollo/javascript/test/cuestionario.js
+++ Desarrollo/javascript/test/cuestionario.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-    console.log("fdgdfag");
 
     //declaracion de los gurpos 
     var group1 = document.getElementsByName("respuestas");
@@ -8,7 +7,43 @@ window.onload = function () {
     var group4 = document.getElementsByName("respuestas4");
     var resultadobtn = document.getElementById("resultadobtn");
     var labelrespuesta = document.getElementById("resultado");
-    var empezarcues =document.getElementById("empezar");
+    var empezarcues = document.getElementById("empezar");
+    var divformulario = document.getElementById("divformulario");
+    //label para las preguntas
+    var labeldiv = document.getElementById("labeldiv");
+    var check = document.getElementById("check");
+    //botones siguietne y atras
+    var volver = document.getElementById("idvolver");
+    var siguiente = document.getElementById("idsiguiente");
+
+    //array de preguntas
+    const arraypreguntas = ["¿Pregunta 1?",
+        "¿Pregunta 2?",
+        "¿Pregunta 3?",
+        "¿Pregunta 5?",
+        "¿Pregunta 6?",
+        "¿Pregunta 7?",
+    ];
+    const arrayrespuestas = [
+        ["Respuesta A", "Respuesta B", "Respuesta C", "Respuesta D"],
+        ["Respuesta A2", "Respuesta B2", "Respuesta C2", "Respuesta D2"],
+        ["Respuesta A3", "Respuesta B3", "Respuesta C3", "Respuesta D3"],
+        ["Respuesta A4", "Respuesta B4", "Respuesta C4", "Respuesta D4"],
+        ["Respuesta A5", "Respuesta B4", "Respuesta C4", "Respuesta D4"],
+        ["Respuesta A6", "Respuesta B4", "Respuesta C4", "Respuesta D4"],
+        ["Respuesta A7", "Respuesta B4", "Respuesta C4", "Respuesta D4"],
+    ];
+
+    const valuerespuestas = ["A",
+        "B",
+        "C",
+        "D",
+    ];
+
+    //contador de preguntas
+    var cont_preg = 0;
+    //contador delos valores de las repsuestas
+    var cont_value = 0;
 
     //contador a cero
     var contador = 0;
@@ -24,32 +59,87 @@ window.onload = function () {
     var respuestas2 = "Respuesta 2";
     var respuestas3 = "Respuesta 3";
     var respuestas4 = "Respuesta 4";
-    
+
+
+    /*muestro la primer pregunta*/
+    sacarpregunta = arraypreguntas[0];
+    console.log(sacarpregunta);
+    labeldiv.innerHTML = sacarpregunta;
+    //funcion de mostrar las respuestas
+    pasar_checkbox(cont_preg);
+
+    //boton siguiente
+    botonsiguiente();
+    btonvolver();
+
+
+    /*fuction del boton volver  */
+    function btonvolver() {
+        volver.addEventListener("click", () => {
+            var sacarpregunta = "";
+            cont_preg--;
+            sacarpregunta = arraypreguntas[cont_preg];
+            console.log(sacarpregunta);
+            labeldiv.innerHTML = sacarpregunta;
+            pasar_checkbox(cont_preg);
+
+        });
+    }
+
+    /*function del boton siguiente */
+    function botonsiguiente() {
+        siguiente.addEventListener("click", () => {
+            var sacarpregunta = "";
+            cont_preg++;
+            sacarpregunta = arraypreguntas[cont_preg];
+            console.log(sacarpregunta);
+            labeldiv.innerHTML = sacarpregunta;
+            pasar_checkbox(cont_preg);
+
+        });
+    }
+
+    /*muestro las respuestas segun las preguntas */
+    function pasar_checkbox(cont_preg) {
+        check.innerHTML = "";
+        cont_value = 0;
+        /*muestro los checkbox */
+        for (let i = 0; i < arrayrespuestas[cont_preg].length; i++) {
+
+            console.log("respuestas " + arrayrespuestas[cont_preg][i]);
+            check.innerHTML += "<input name='" + cont_preg + "' type='checkbox' value='" + valuerespuestas[cont_value] + "'>" + arrayrespuestas[cont_preg][i];
+            cont_value++;
+        }
+    }
+
+
+
+
+
 
     //bton btn resultado
     btnresultado();
-
 
     function btnresultado() {
 
         resultadobtn.addEventListener('click', () => {
             //funciones de los check
-           checkpreguntas1();
-           checkpreguntas2();
-           checkpregunta3();
-           checkpregunta4();
-           
+            checkpreguntas1();
+            checkpreguntas2();
+            checkpregunta3();
+            checkpregunta4();
 
 
-        //resultado dependiendo las respuestas
-        if (contador == contador_respuestasA) {
-            console.log(respuestas + "contiene "+ contador + "sleccionados");
 
-        }
-        if (contador2 == contador_respuestasB) {
+            //resultado dependiendo las respuestas
+            if (contador == contador_respuestasA) {
+                console.log(respuestas + "contiene " + contador + "sleccionados");
 
-            console.log(respuestas2 + "contiene "+ contador2 + "sleccionados");
-        }
+            }
+            if (contador2 == contador_respuestasB) {
+
+                console.log(respuestas2 + "contiene " + contador2 + "sleccionados");
+            }
 
         })
     }
@@ -59,100 +149,102 @@ window.onload = function () {
         for (let i = 0; i < group1.length; i++) {
             //si esta selecionado , me nuestar el value
             if (group1[i].checked) {
-                
+
                 if (group1[i].value == "a") {
                     contador++;
                     console.log(group1[i].value);
-                    console.log("contador A tiene"+ contador);
+                    console.log("contador A tiene" + contador);
                 }
                 if (group1[i].value == "b") {
                     contador2++;
                     console.log(group1[i].value);
-                    console.log("contador B tiene "+ contador2);
+                    console.log("contador B tiene " + contador2);
                 }
                 if (group1[i].value == "c") {
                     contador3++;
                     console.log(group1[i].value);
-                    console.log("contador C tiene"+ contador3);
+                    console.log("contador C tiene" + contador3);
                 }
             }
 
         }
     }
+
     function checkpreguntas2() {
         for (let j = 0; j < group2.length; j++) {
             if (group2[j].checked) {
-               
+
                 if (group2[j].value == "a") {
                     contador++;
                     console.log(group2[j].value);
-                    console.log("contador A2 tiene"+ contador);
+                    console.log("contador A2 tiene" + contador);
 
                 }
                 if (group2[j].value == "b") {
                     contador2++;
                     console.log(group2[j].value);
-                    console.log("contador B2 tiene"+ contador2);
+                    console.log("contador B2 tiene" + contador2);
 
                 }
                 if (group2[j].value == "c") {
                     contador3++;
                     console.log(group2[j].value);
 
-                    console.log("contador C2 tiene "+ contador3);
+                    console.log("contador C2 tiene " + contador3);
 
                 }
             }
 
         }
     }
+
     function checkpregunta3() {
-        
+
         for (let k = 0; k < group3.length; k++) {
-          if (group3[k].checked) {
-              
-            if (group3[k].value == "a") {
-                contador++;
-                console.log(group3[k].value);
+            if (group3[k].checked) {
 
-                console.log("contador A3 tiene"+ contador);
+                if (group3[k].value == "a") {
+                    contador++;
+                    console.log(group3[k].value);
 
+                    console.log("contador A3 tiene" + contador);
+
+                }
+                if (group3[k].value == "b") {
+                    contador2++;
+                    console.log(group3[k].value);
+                    console.log("contador B3 tiene" + contador2);
+
+                }
+                if (group3[k].value == "c") {
+                    contador3++;
+                    console.log(group3[k].value);
+                    console.log("contador C3 tiene " + contador3);
+
+                }
             }
-            if (group3[k].value == "b") {
-                contador2++;
-                console.log(group3[k].value);
-                console.log("contador B3 tiene"+ contador2);
 
-            }
-            if (group3[k].value == "c") {
-                contador3++;
-                console.log(group3[k].value);
-                console.log("contador C3 tiene "+ contador3);
-
-            }
-          }
-            
         }
     }
 
     function checkpregunta4() {
         for (let l = 0; l < group4.length; l++) {
-            
+
             if (group4[l].checked) {
                 if (group4[l].value == "a") {
                     contador++;
                     console.log(group4[l].value);
-                    console.log("contador A4 tiene"+ contador);
+                    console.log("contador A4 tiene" + contador);
                 }
                 if (group4[l].value == "b") {
                     contador2++;
                     console.log(group4[l].value);
-                    console.log("contador B4 tiene"+ contador2);
+                    console.log("contador B4 tiene" + contador2);
                 }
                 if (group4[l].value == "c") {
                     contador3++;
                     console.log(group4[l].value);
-                    console.log("contador C4 tiene"+ contador3);
+                    console.log("contador C4 tiene" + contador3);
                 }
             }
         }
