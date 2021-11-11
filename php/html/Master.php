0e4cab1d05d15665824bad6f9f67d6ea9cc738c4
antcha19 <you@example.com>
2021-11-11 13:25:26 +0100

    regfsg

--- php/html/Master.php
+++ php/html/Master.php
@@ -2,21 +2,55 @@
 class Master
 {
 
-    //propiedad 
-    public $numero_ale;
-    public $num_intentos;
-    function __construct()
-    {
-    }
+  //propiedad 
+  public $numero_ale;
+  public $numero_usuarios;
+  function __construct()
+  {
+    //creo el array vacio
+    $this->numero_usuarios = [];
+  }
+  
 
 
-    //funcio numero aleatorio
-    function n_aleatorio(){
-        //array vacio
-        $this->numero_ale = [];
-        $numeroramdom = rand(1,6);
-        for ($i=0; $i <4 ; $i++) { 
-          array_push($numeroramdom);
-        }
+  //funcio numero aleatorio
+  function n_aleatorio()
+  {
+    //array vacio
+    $this->numero_ale = [];
+    //creo el primer numero aleatorio
+    $numeroramdom = rand(1, 6);
+    //inserto al array
+    array_push($this->numero_ale, $numeroramdom);
+    //mientras array de numero_aleatorio sea de longuitud de 4
+    while (count($this->numero_ale) != 4) {
+      //siguiente numero aleatorio 
+      $numeroramdom = rand(1, 6);
+      //si el valor existe continua sin hacer nada
+      if (in_array($numeroramdom, $this->numero_ale)) {
+        continue;
+      } else {
+        //añado al array el numero aleatorio si no existe
+        array_push($this->numero_ale, $numeroramdom);
+      }
     }
+    //paso el array a string
+    $this->numero_ale = implode($this->numero_ale);
+  }
+
+
+
+  function getn_aleatorio()
+  {
+    return $this->numero_ale;
+  }
+
+  //añado los numeros insertados por el usuario
+  function setn_usuarios($num_insertado){
+    array_push($this->numero_usuarios,$num_insertado);
+  }
+  //para mostrar el array 
+  function getn_usuarios(){
+    return $this->numero_usuarios;
+  }
 }
--- php/html/index.php
+++ php/html/index.php
@@ -1,27 +1,51 @@
-<?php 
+<?php
+
+if (!isset($_SESSION['master'])) {
+    $_SESSION['master'] = new Master();
+    //funcion numero aletorio;
+    $_SESSION['master']->n_aleatorio();
+}
+//importa la cabecera 
 include 'master_cabecera.php';
 /*se ejecuta cuando necesito una class*/
-function __autoload($class){
-    require_once $class.'.php';
+function __autoload($class)
+{
+    require_once $class . '.php';
 }
-
-
 ?>
-    
-    <?php 
+<form method="get" enctype="multipart/form-data">
+    <input type="number" min="1" pattern=".{4,} name="numero_insertado">
+    <input type="submit" value="comprobar" name="comprobar">
+    <br>
+    <?php
+    if (isset($_GET['comprobar'])) {
+        echo "clic en comprobar <br>";
+        if ($_GET['numero_insertado']) {
+            //inserto los numero inotrducidos al array
+            $_SESSION['master']->setn_usuarios($_GET['numero_insertado']);
+        }
+    }
+    //imprimero los valores del array 
+    foreach ($_SESSION['master']->getn_usuarios() as $key => $value) {
+        echo $value."<br>";
+    }
+    //nuestro los numero aleatorios
+    echo "Numero aleatorio:" . $_SESSION['master']->getn_aleatorio() . "<br>";
+
+  ;
 
 
-?>
-    
+    ?>
 
 
 
+</form>
 
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
