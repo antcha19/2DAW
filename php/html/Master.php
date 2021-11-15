6ebf4b01e1fa8399ad1f39981bbdf4d1f47d92f6
antcha19 <you@example.com>
2021-11-15 13:48:10 +0100

    sdfsdf

--- php/html/Master.php
+++ php/html/Master.php
@@ -5,12 +5,14 @@ class Master
   //propiedad 
   public $numero_ale;
   public $numero_usuarios;
+  public $muertos = 0;
+  public $tocados = 0;
   function __construct()
   {
     //creo el array vacio
     $this->numero_usuarios = [];
   }
-  
+
 
 
   //funcio numero aleatorio
@@ -46,11 +48,56 @@ class Master
   }
 
   //añado los numeros insertados por el usuario
-  function setn_usuarios($num_insertado){
-    array_push($this->numero_usuarios,$num_insertado);
+  function setn_usuarios($num_insertado)
+  {
+    array_push($this->numero_usuarios, $num_insertado);
   }
   //para mostrar el array 
-  function getn_usuarios(){
+  function getn_usuarios()
+  {
     return $this->numero_usuarios;
+  }
+
+  function comprobar()
+  {
+//reinicio las variables para la siguiente comprobacion
+$this->muertos =0;
+$this->tocados =0;
+    //paso a un array para comprobar
+    $array_aleatorio = str_split($this->numero_ale);
+    //obtenlo los ultimos
+    $ultimaposicion = end($this->numero_usuarios);
+    //paso a un array 
+    $array_numerosintro = str_split($ultimaposicion);
+    for ($i = 0; $i < count($array_aleatorio); $i++) {
+      //echo $array_aleatorio[$i];
+      for ($j = 0; $j < count($array_numerosintro); $j++) {
+        // echo $array_numerosintro[0];
+        //si son iguales entra
+        if ($array_aleatorio[$i] == $array_numerosintro[$j]) {
+          //si las psosicones son iugales me suma un muerto
+          if ($i == $j) {
+            $this->muertos++;
+          }
+          //si las posiciones son distinas es un tocado
+          if ($i != $j) {
+            $this->tocados++;
+          }
+        }
+      }
+    }
+  }
+
+  //muestro los muertos
+  function getmuertos()
+  {
+    
+    return $this->muertos;
+  }
+  //muestro los tocados
+  function gettocados()
+  {
+    
+    return $this->tocados;
   }
 }
--- php/html/index.php
+++ php/html/index.php
@@ -1,10 +1,5 @@
 <?php
 
-if (!isset($_SESSION['master'])) {
-    $_SESSION['master'] = new Master();
-    //funcion numero aletorio;
-    $_SESSION['master']->n_aleatorio();
-}
 //importa la cabecera 
 include 'master_cabecera.php';
 /*se ejecuta cuando necesito una class*/
@@ -12,40 +7,61 @@ function __autoload($class)
 {
     require_once $class . '.php';
 }
+//sesion inicada
+session_start();
+if (!isset($_SESSION['master'])) {
+    $_SESSION['master'] = new Master();
+    //funcion numero aletorio;
+    $_SESSION['master']->n_aleatorio();
+}
 ?>
 <form method="get" enctype="multipart/form-data">
-    <input type="number" min="1" pattern=".{4,} name="numero_insertado">
-    <input type="submit" value="comprobar" name="comprobar">
-    <br>
+    <div class="centro">
+        <input type="number" name="numero_insertado">
+        <input type="submit" value="comprobar" name="comprobar">
+        <br>
+    </div>
+
     <?php
     if (isset($_GET['comprobar'])) {
-        echo "clic en comprobar <br>";
+
         if ($_GET['numero_insertado']) {
             //inserto los numero inotrducidos al array
             $_SESSION['master']->setn_usuarios($_GET['numero_insertado']);
+            echo  $_SESSION['master']->comprobar() . "<br>";
+            echo  "Muertos: " . $_SESSION['master']->getmuertos() . "<br>";
+            echo  "Tocados: " . $_SESSION['master']->gettocados() . "<br>";
         }
     }
-    //imprimero los valores del array 
-    foreach ($_SESSION['master']->getn_usuarios() as $key => $value) {
-        echo $value."<br>";
+    //imprimo los valores del array
+    $valores_array = "";
+    
+    foreach ($_SESSION['master']->getn_usuarios() as $key => $valores) {
+        $valores_array .=  $valores . "<br>";
     }
+    echo "<div>".$valores_array."<div>";
     //nuestro los numero aleatorios
-    echo "Numero aleatorio:" . $_SESSION['master']->getn_aleatorio() . "<br>";
-
-  ;
-
-
+    echo "<input type=submit value=Reiniciar name=reiniciar>";
+    //cuando gana muestra 
+    if ($_SESSION['master']->getmuertos() == 4) {
+        echo "<div> Has ganado </div>";
+        echo " Numero aleatorio : <br>";
+        echo "<label id=label>" . $_SESSION['master']->getn_aleatorio() . "</label><br>";
+    }
+    //reiniciar
+    if (isset($_GET['reiniciar'])) {
+        session_destroy();
+        header('Location: index.php');
+    }
     ?>
-
-
-
 </form>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
