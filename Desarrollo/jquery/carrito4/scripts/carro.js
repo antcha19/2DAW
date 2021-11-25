cdd59df37817354bb7d57a05054c98ad5ea9b707
antcha19 <you@example.com>
2021-11-25 15:00:38 +0100

    erter

---
+++ Desarrollo/jquery/carrito4/scripts/carro.js
@@ -0,0 +1,217 @@
+$(document).ready(function () {
+
+
+
+  //parte 1
+  /* //cambia de color al item es una class
+    $(".item").css('background-color','#cecece' );
+    $("#cart_items").css('border','4px solid black');
+    $("img").css('border','1px solid blue');
+    //hijos directos
+    $(".item > label ").css('text-decoration', 'underline');
+    // color rojo a todos los botones que estan dentro del cart-....// 
+    $("#cart_container  button").css('color', 'red');
+    $(".item   label + label").css('color', 'white');
+    //todos los input de la pagina
+    //color de verde a todos los que contengas 
+    $(":contains('€'), input").css('color','green');
+    //cambia color al div vacio
+    $("div:empty").css('background-color', 'yellow');
+    //cambia color al primero y al ultimo
+    $(".item:first, .item:last").css('background-color', 'red');
+    //border al img que sea una imagen
+    $("img[src*='camiseta']").css('border-color','green');
+   */
+
+  //parte 3
+  $(function () {
+    var $car = $("#cart_items");
+    var pos_ori = $car.offset();
+    let ancho_ori = $car.width();
+    //guardo en una variable la posicion izquierda
+    var pos_izquierda = pos_ori.left;
+
+    // sumanos el pos_izquierda mas el ancho del div
+    var pos_derecha = pos_izquierda + $car.width();
+
+    var total = parseInt(pos_derecha) + parseInt(pos_izquierda);
+
+
+    $("#btn_clear").bind("click", function () {
+      //click a todos las class .delete
+      $(".delete").trigger("click");
+      var $cart_items = $("#cart_items");
+      var pos = $cart_items.offset();
+      //iguala la posicion 
+      pos.left = pos.left;
+      $cart_items.offset(pos);
+    })
+
+
+    $("#btn_prev").bind("click", function () {
+      var $car = $("#cart_items");
+      var pos = $car.offset();
+      console.log(pos_izquierda);
+      console.log(pos.left);
+      if (pos_izquierda > pos.left) {
+        pos.left += 50;
+        $car.offset(pos);
+      }
+    });
+
+
+    $("#btn_next").bind("click", function () {
+      var pos = $("#cart_items").offset();
+      var $width = $("#cart_items").width();
+      pos.left -= 50;
+      if ($width + pos.left > pos_ori.left + ancho_ori) {
+        $("#cart_items").offset(pos);
+      }
+    });
+
+
+    //oculto la barra comprar
+    ocultarbotones();
+
+
+
+  })
+
+
+  //parte 2
+  dobleclick();
+
+
+  function dobleclick() {
+
+    //doble click al elemento
+
+    $(".item").bind("dblclick", function () {
+      //le paso el this;
+      actualizar_stock($(this), 1);
+      add_carrito($(this));
+    })
+  }
+
+  //$item es el this del
+  function actualizar_stock($item, valor) {
+    //busco dentro del item el strock
+    $stock = $item.find(".stock");
+    $stocktext = $stock.text();
+    //numero del stock
+    let stocksub = $stocktext.substring(6);
+    if (stocksub > 0) {
+      //cantidad de productos 
+      var numArticulosCarrito = $("#cart_items").children().length
+      if (numArticulosCarrito > 3) {
+        $("#cart_items").width($("#cart_items").width() + 120);
+
+      }
+
+      var stocknuevo = stocksub - (valor);
+      //modificado el stock 
+      $stock.text("Stock " + stocknuevo);
+      //imcrementamos las compras
+      let $cantidad = $("#citem").val(parseInt($("#citem").val()) + 1);
+      let $value_cantidad = $cantidad.val();
+      //sumanos el precio
+      $precio = $item.find(".price").text();
+      let espacio = $precio.indexOf(" ");
+      let precio = $precio.substring(0, espacio);
+      //agotado
+      if (stocknuevo == 0) {
+        $stock.addClass("agotado");
+      }
+      //suma cantidad total de la compras
+      $("#cprice").val(parseInt($("#cprice").val()) + parseInt(precio) + " €");
+      //mostramos botones 
+  
+      if ($value_cantidad > 0) {
+       
+        $("#btn_comprar").show();
+        $("#btn_clear").show();
+      }
+
+
+
+    }
+  }
+
+  function add_carrito($item) {
+    //elemento clonado
+    $clonado = $item.clone().attr("id", "C" + $item.attr("id"));
+    //Le añadiremos la clase icart a la copia creada
+    $clonado.addClass("icart");
+    //elemento de la clase stock
+    $clonado.find(".stock").hide();
+
+    //Cambiaremos la propiedad css cursor del elemento
+    $clonado.css("cursor", "default");
+
+    $delete = $('<a  href="" class="delete"></a>');
+    $clonado.append($delete);
+    //añado a cart_items el clonado
+    $("#cart_items").append($clonado);
+
+
+    $delete.on("click", function () {
+      var numArticulosCarrito = $("#cart_items").children().length;
+
+      //elemento pardre del enlace
+      var idpadre = $(this).parent().attr("id");
+      //id de la lista arituclos original
+      var id = idpadre.substring(1);
+
+      //saca el stock  del productos a borrar con la id
+      $stock = $("#" + id).find(".stock");
+      $s_texto = $stock.text();
+
+      //cantidad del stock
+      let stocksub = $s_texto.substring(6);
+      let parse = parseInt(stocksub);
+      //hago la suma
+      let suma = parse + 1;
+      //cambio el stock
+      $stock.text("Stock " + suma);
+      //quito la class agoatado
+      if (parse == 0) {
+        $stock.removeClass("agotado");
+      }
+      //actuliazamos la compra
+      $("#citem").val(parseInt($("#citem").val()) - 1);
+      //actualizamos el precio $precio esta declarado en actualizar_stock
+      $precio_quitar = $item.find(".price").text();
+      let espacio = $precio_quitar.indexOf(" ");
+      let precio = $precio_quitar.substring(0, espacio);
+      $("#cprice").val(parseInt($("#cprice").val()) - parseInt(precio) + " €");
+      //eliminamos el articulo de la compra
+      $("#C" + $item.attr("id")).remove();
+      
+      //borrar 120pix
+      if (numArticulosCarrito  > 4) {
+        
+        $("#cart_items").width($("#cart_items").width() - 120);
+      }
+
+       numArticulosCarrito = $("#cart_items").children().length;
+       console.log(numArticulosCarrito);
+      if (numArticulosCarrito == 0) {
+        ocultarbotones();
+      }else if (numArticulosCarrito < 4 ) {
+        
+      }
+      return false;
+    })
+
+  }
+
+  function ocultarbotones() {
+
+    $("#btn_comprar").hide();
+    $("#btn_clear").hide();
+    $("#btn_prev").hide();
+    $("#btn_next").hide();
+  }
+
+
+})
---
+++ Desarrollo/jquery/carritoAna/scripts/carro.js
@@ -0,0 +1,148 @@
+﻿var ordenCarrito = 0;
+var posCarritoInicial;
+var anchoCarritoInicial;
+var anchoProductoEnCarrito = 120;
+
+function actualizaStockProducto($item, incremento) {
+	var stock = parseInt($item.children(".stock").html().replace("Stock ", ""));
+
+	if (stock + incremento >= 0) {
+		stock += incremento;
+		$item.children(".stock").html("Stock " + stock);
+		if (stock == 0) {
+			$item.find(".stock").addClass("agotado");
+			$item.unbind("dblclick");
+		} else {
+			if (stock == 1 && incremento == 1) {
+				$item.find(".stock").removeClass("agotado");
+				establece_evento_dblclick_items($item);
+			}
+		}
+	}
+}
+
+function actualizaNumeroProductosPedidos(incremento) {
+	var numProductosPedido = parseInt($("#citem").val());
+	numProductosPedido += incremento;
+	$("#citem").val(numProductosPedido);
+}
+
+function actualizaPrecioTotal($item, incremento) {
+	var precioPedido = parseInt($("#cprice").val());
+	precioPedido += parseInt(incremento);
+	$("#cprice").val(precioPedido + " €");
+}
+
+function incrementaAnchoCarrito(incremento) {
+	$("#cart_items").width($("#cart_items").width() + incremento);
+}
+
+function anyadeProductoAlCarrito($item) {
+	var $delete = $('<a href="" class="delete"></a>');
+
+	var id = "c" + $item.attr("id");
+	$copia = $item.clone().attr("id", id).addClass('icart').prepend($delete);
+	$copia.children(":not(a)").andSelf().css("cursor", "default").find(".stock").hide();
+
+	$("#cart_items").prepend($copia);
+}
+
+function desplazaCarritoIzquierda(desplazamiento) {
+	var pos = $("#cart_items").offset();
+
+	if (pos.left + desplazamiento <= posCarritoInicial.left)
+		pos.left += desplazamiento;
+	else
+		pos.left = posCarritoInicial.left;
+
+	$("#cart_items").offset(pos);
+}
+
+function desplazaCarritoDerecha(desplazamiento) {
+	var pos = $("#cart_items").offset();
+	var ancho = $("#cart_items").width();
+	var der = pos.left + ancho;
+
+	if (der - desplazamiento >= posCarritoInicial.left + anchoCarritoInicial)
+		pos.left -= desplazamiento;
+	else
+		pos.left = posCarritoInicial.left + anchoCarritoInicial - ancho;
+
+	$("#cart_items").offset(pos);
+}
+
+function establece_evento_dblclick_items($items) {
+	$items.dblclick(function () {
+		actualizaStockProducto($(this), -1);
+
+		actualizaNumeroProductosPedidos(1);
+
+		actualizaPrecioTotal($(this), parseInt($(this).children(".price").html()));
+
+		anyadeProductoAlCarrito($(this));
+
+		var numArticulosCarrito = $("#cart_items").children().length;
+		console.log(numArticulosCarrito);
+		if (numArticulosCarrito > 4) {
+			incrementaAnchoCarrito(anchoProductoEnCarrito);
+		}
+		//ocultar barra comprar
+		if (numArticulosCarrito < 0) {
+			$("#nav_left").hide();
+		}
+
+	});
+}
+
+function eliminaProductoDelCarrito($item) {
+	var id = $item.attr("id");
+	id = id.substring(1);
+
+	actualizaStockProducto($("#" + id), 1);
+
+	actualizaNumeroProductosPedidos(-1);
+
+	actualizaPrecioTotal($item, -parseInt($item.children(".price").html()));
+
+	var pos = $("#cart_items").offset();
+
+	var numArticulosCarrito = $("#cart_items").children().length - 1;
+	if (numArticulosCarrito >= 4) {
+		incrementaAnchoCarrito(-anchoProductoEnCarrito);
+
+		var anchoCarrito = $("#cart_items").width();
+		var der = pos.left + anchoCarrito;
+		if (der < posCarritoInicial.left + anchoCarritoInicial)
+			pos.left = posCarritoInicial.left;
+	} else
+		pos.left = posCarritoInicial.left;
+
+	$("#cart_items").offset(pos);
+
+	$item.remove();
+}
+
+$(function () {
+	anchoCarritoInicial = $("#cart_items").width();
+	posCarritoInicial = $("#cart_items").offset();
+
+	establece_evento_dblclick_items($(".item"));
+
+	$(document).on("click", ".delete", function () {
+		eliminaProductoDelCarrito($(this).parent());
+
+		return false;
+	});
+
+	$("#btn_clear").click(function (evento) {
+		$(".delete").trigger("click");
+	});
+
+	$("#btn_prev").click(function (evento) {
+		desplazaCarritoIzquierda(50);
+	});
+
+	$("#btn_next").click(function (evento) {
+		desplazaCarritoDerecha(50);
+	});
+});
