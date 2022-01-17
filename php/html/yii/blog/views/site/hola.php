e4333b5927f07cec0644ab9d4d5107bc7dc15c82
antcha19 <you@example.com>
2022-01-17 13:07:44 +0100

    arettk

---
+++ php/html/yii/blog/views/site/hola.php
@@ -0,0 +1,13 @@
+<?php
+                            
+
+use yii\helpers\Html;
+                            
+
+?>
+                            
+
+<h1>Hola <?= Html::encode($target) ?></h1>
+                            
+
+<p>Bienvenido a tu demostración de Yii2.</p>
